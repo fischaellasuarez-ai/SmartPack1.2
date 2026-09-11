5eb85b663adb3870574c8ef7d16f299d9f4f6f59
Suarez_MiniPeta1 <fischaellasuarez@gmail.com>
2026-09-11 20:50:22 +0800

    Pecore_MiniPeta1

---
+++ app/src/test/java/com/example/smartpack/Pecore_MiniPeta1.java
@@ -0,0 +1,18 @@
+package com.example.smartpack;
+
+import org.junit.Test;
+
+public class Pecore_MiniPeta1 {
+    @Test
+    public void printMyProfile() {
+        String myName = "Nick";
+        String petName = "Yuri and Cassie";
+        String favFood = "FriedChicken";
+        int myAge = 15;
+
+        System.out.println("<My DIGITAL Profile>");
+        System.out.println("Hello! You may call me " + myName + " and currently, " + myAge + " years old.");
+        System.out.println("I have a 2 adorable cats named " + petName + ".");
+        System.out.println("the food i craved the most is " + favFood + ".");
+    }
+}
